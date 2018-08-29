3282f1ca09990b89db8dad240c4b0e415400a748
nickbrowne <nickbrowne@users.noreply.github.com>
2018-08-29 18:49:52 +1000

    Close socket

--- client.rb
+++ client.rb
@@ -2,9 +2,12 @@ require 'socket'
 
 s = TCPSocket.new('localhost', 1234)
 
-(0..999999).lazy.each do |i|
-  s.puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa #{i}"
-  print "."
-  # s.close
-  sleep 1
+begin
+  (0..999999).lazy.each do |i|
+    s.puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa #{i}"
+    print "."
+    sleep 1
+  end
+ensure
+  s.close
 end
